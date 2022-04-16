b50917c4643e7919d1a0acda13e32c5cfb7f5e6e
amhuang <amhuang00@gmail.com>
2022-04-16 15:01:38 -0400

    navbar

--- server.py
+++ server.py
@@ -7,6 +7,14 @@ app = Flask(__name__)
 @app.route('/')
 def home():
    return render_template('home.html')
-    
+
+@app.route('/learn/')
+def learn():
+   return render_template('learn.html')
+
+@app.route('/quiz/')
+def quiz():
+   return render_template('quiz.html')
+
 if __name__ == '__main__':
    app.run(debug = True)
