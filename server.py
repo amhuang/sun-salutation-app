986dc3ef3398f8ca5feaf898c59203eeee272be7
amhuang <amhuang00@gmail.com>
2022-04-18 10:03:11 -0400

    cleaned up quiz data

--- server.py
+++ server.py
@@ -8,19 +8,6 @@ import random
 
 app = Flask(__name__)
 
-'''
-QUIZ_QUESTIONS = {
-    1: {
-        "id": 1,
-        "len": QUIZ_LEN,
-        "type": "matching",
-        "answers": ["Cobra", "Downward Facing Dog", "Eight Point Salute", "Equestrian (L)", "Half Forward Bend", "Plank", "Mountain Pose", "Raised Arm"],
-        "imgs": [],
-        "user_data": quiz_responses[id] if id in quiz_responses else "",
-    }
-}
-'''
-
 data = {
    "1": {
       "id": "1",
@@ -28,7 +15,6 @@ data = {
       "Muscles": ["Pelvis", "Legs"],
       "img": "/static/img/poses/mountain",
       "gif": "/static/img/GIFs/1-mountain",
-
    },
 
    "2": {
@@ -122,8 +108,46 @@ data = {
 }
 
 # Quiz data
-quiz_responses = {}
 QUIZ_LEN = 3
+quiz_data = {
+    1: {
+        "id": 1,
+        "len": QUIZ_LEN,
+        "type": "matching",
+        "question": "Match the name to the pose.",
+        "answers": ["Cobra", "Downward Facing Dog", "Eight Point Salute", "Equestrian (L)", "Half Forward Bend", "Plank", "Mountain Pose", "Raised Arm"],
+        "imgs": ["/static/img/poses/Cobra.jpeg", "/static/img/poses/Downward Facing Dog.jpeg", "/static/img/poses/Eight Point Salute.jpeg", "/static/img/poses/Equestrian (L).jpeg", "/static/img/poses/Half Forward Bend.jpeg", "/static/img/poses/Plank.jpeg", "/static/img/poses/Mountain Pose.jpeg", "/static/img/poses/Raised Arm.jpeg"],
+        "user_data": "",
+    },
+    2: {
+        "id": 2,
+        "len": QUIZ_LEN,
+        "type": "ordering",
+        "question": "Match the name to the pose.",
+        "answers": ["Prayer Pose", "Raised Arm", "Half Forward Bend", "Equestrian (L)", "Plank", "Eight Point Salute"],
+        "imgs": ["/static/img/poses/Prayer Pose.jpeg", "/static/img/poses/Raised Arm.jpeg", "/static/img/poses/Half Forward Bend.jpeg", "/static/img/poses/Equestrian (L).jpeg", "/static/img/poses/Plank.jpeg", "/static/img/poses/Eight Point Salute.jpeg"],
+        "user_data": "",
+    },
+    3: {
+        "id": 3,
+        "len": QUIZ_LEN,
+        "type": "matching",
+        "question": "Match the name to the pose.",
+        "answers": ["Cobra", "Downward Facing Dog", "Eight Point Salute", "Equestrian (L)", "Half Forward Bend", "Plank", "Mountain Pose", "Raised Arm"],
+        "imgs": ["/static/img/poses/Cobra.jpeg", "/static/img/poses/Eight Point Salute.jpeg", "/static/img/poses/Equestrian (L).jpeg", "/static/img/poses/Half Forward Bend.jpeg", "/static/img/poses/Plank.jpeg", "/static/img/poses/Mountain Pose.jpeg", "/static/img/poses/Raised Arm.jpeg"],
+        "user_data": "",
+    },
+    4: {
+        "id": 4,
+        "len": QUIZ_LEN,
+        "type": "muscle",
+        "question": "Select the muscles activated in raised arm pose.",
+        "answers": ["Hamstrings","Calves", "Spine", "Chest", "Shoulders"],
+        "imgs": [],
+        "user_data": "",
+    }
+}
+
 
 @app.route('/')
 def home():
@@ -144,52 +168,18 @@ def quiz(id=None):
     global quiz_responses
     id = int(id)
     if request.method == 'GET':
-        data = get_quiz_data(id)
+        data = quiz_data[id] #get_quiz_data(id)
         print("getting data","\n", data)
         return render_template('quiz.html', data=data)
     else:
         answer = request.get_json()
-        quiz_responses[id] = answer
-        print("quiz_responses","\n",  quiz_responses)
-        data = get_quiz_data(id)
+        print("answer", "\n", answer)
+        quiz_data[id]["user_data"] = answer
+        #quiz_responses[id] = answer
+        print("quiz_responses","\n",  quiz_data[id]["user_data"])
+        data = quiz_data[id] # get_quiz_data(id)
         print("update ddata","\n",  data)
         return jsonify(data=data)
-
-def get_quiz_data(id):
-    # Pose name matching question
-    if id == 1:
-        type = "matching"
-        question = "Match the name to the pose."
-        answers = ["Cobra", "Downward Facing Dog", "Eight Point Salute", "Equestrian (L)", "Half Forward Bend", "Plank", "Mountain Pose", "Raised Arm"]
-        img_paths = ["/static/img/poses/" + pose + ".jpeg" for pose in answers]
-
-    # Sun salutation order question
-    elif id == 2:
-        type = "ordering"
-        question = "Drag the poses in order for moves 1-6 of a sun salutation"
-        answers = ["Prayer Pose", "Raised Arm", "Half Forward Bend", "Equestrian (L)", "Plank", "Eight Point Salute"]
-        img_paths = ["/static/img/poses/" + pose + ".jpeg" for pose in answers]
-
-    elif id == 3:
-        type = "ordering"
-        question = "Drag the poses in order for moves 7-12 of a sun salutation"
-        answers = ["Cobra", "Downward Facing Dog", "Equestrian (R)", "Half Forward Bend", "Raised Arm", "Mountain Pose"]
-        img_paths = ["/static/img/poses/" + pose + ".jpeg" for pose in answers]
-
-    # Muscles used in a pose question
-    elif 4 <= id and id < 12:
-        type = "muscles"
-
-    data = {
-        "id": id,
-        "len": QUIZ_LEN,
-        "type": type,
-        "imgs": img_paths,
-        "question": question,
-        "answers": answers,
-        "user_data": quiz_responses[id] if id in quiz_responses else "",
-    }
-    return data
 
 @app.route('/quiz_result', methods = ['GET'])
 def quiz_results():
@@ -202,14 +192,14 @@ def quiz_results():
         elif val["type"] == "ordering":
             ordering_score += val["score"]
 
-    data = {
+    quiz_results = {
         "prev": QUIZ_LEN,
         "matching_score": matching_score,
         "matching_total": 8,
         "ordering_score": ordering_score,
         "ordering_total": 12,
     }
-    return render_template('quiz_result.html', data=data)
+    return render_template('quiz_result.html', data=quiz_results)
 
 if __name__ == '__main__':
    app.run(debug = True)
