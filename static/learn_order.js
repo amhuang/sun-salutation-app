832c9fe8c69b5bfb8f8cda83e1f2accbc899c4aa
Jenny Liu <jennyliu887@gmail.com>
2022-05-02 14:23:09 -0400

    updated colors on learn flash card page

--- static/learn_order.js
+++ static/learn_order.js
@@ -9,35 +9,6 @@ $(document).ready(function(){
 
 len = 12
 
-// function createCards(){
-//   for (i = 1; i<= 12; i++){
-//     let col = $("<div class='col-md-2'>")
-//     let card = $("<div class='flip-card'>")
-//     let inner = $("<div class='flip-card-inner'>")
-//     let front = $("<div class='flip-card-front'>")
-//     let img = $("<img>").attr({src: d[i]["img"], width:'200px'})
-//     let back = $("<div class='flip-card-back'>")
-//     let name = $("<p>")
-//     name.html(d[i]["Name"])
-//     let views = $("<p class = 'normal_p'>")
-//     views.html("Viewed: " + d[i]["views"])
-//
-//     back.append(name)
-//     back.append(views)
-//     front.append(img)
-//     inner.append(front)
-//     inner.append(back)
-//     card.append(inner)
-//     col.append(card)
-//     if (i <= 6){
-//       $("#row1").append(col)
-//     }
-//     else {
-//       $("#row2").append(col)
-//     }
-//   }
-// }
-
 // Displays current progress in the progress bar
 function displayProgress() {
     prog = $(".progress-bar")
